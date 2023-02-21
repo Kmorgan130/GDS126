cf66e54eefd14e45af1f8042714168a86588a85c
keith <kxmorgan@email.neit.edu>
2023-02-21 09:05:09 -0500

    Push1

--- GDS_126_Engine/js/Data/player.js
+++ GDS_126_Engine/js/Data/player.js
@@ -29,8 +29,8 @@ var playerData ={
 			cycle:true,
 			frames:
 			[
-				{width:128, height:128, startX:0, startY:0},
-				{width:128, height:128, startX:128, startY:0}
+				{width:100, height:140, startX:1600, startY:0},
+				{width:100, height:140, startX:1699, startY:0}
 				
 			]
 		},
