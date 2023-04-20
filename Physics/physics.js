9c6abcd98f39333fecac5913236b82ca0f2e494f
keith <kxmorgan@email.neit.edu>
2023-04-20 11:18:40 -0400

    push

--- Physics/physics.js
+++ Physics/physics.js
@@ -5,6 +5,8 @@ var context;
 var timer;
 var interval = 1000/60;
 var player;
+var p1Wins = 0;
+var ball;
 
 //---------------Set Friction and Gravity-----------------
 var frictionX = .85;	
@@ -16,9 +18,24 @@ var gravity = 1;
 
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");	
+
+	
 	
 	player = new GameObject();
-	player.force = 2;
+	player.x = canvas.width/2;
+	player.y = canvas.height - 50;
+	player.height = 40;
+	player.width = 250;
+	player.color= "#00ffff"
+	//player.force = 2;
+
+	ball = new GameObject();
+	ball.x = canvas.width/2;
+	ball.y = canvas.height/2;
+	radius: 40;
+	ball.color = "#ff00ff"
+//
+	
 	
 	timer = setInterval(animate, interval);
 
@@ -27,14 +44,53 @@ function animate()
 {
 	context.clearRect(0,0,canvas.width, canvas.height);	
 	
+
+	if(d)
+	{	
+		player.vx +=  player.ax * player.force;
+	}
+	if(a)
+	{
+		player.vx += player.ax * -player.force;
+	}
+	if(w)
+	{	
+		player.vy += player.ay * -player.force;
+	}
+	if(s)
+	{
+		player.vy += player.ay * player.force;
+	}
+	//---------------------------------------------------------------------------------------
+	player.x += player.vx;
+	player.y += player.vy;
+	
 	//Call just one of the functions below to view acceleration, friction, gravity and pixel lock.
-	showAcceleration();
-	//showFriction();
-	//showGravity();
-	//showPixelLock();
-	//showBounce();
+	
+	
+	context.fillStyle = "#555555"
+    context.font = "16px Arial";
+    context.fillText("Score:", 80, 25);
+
+	context.fillStyle = "#555555"
+    context.font = "16px Arial";
+    context.fillText(p1Wins, 130, 25);
+
+	context.save();
+	context.strokeStyle = "black";
+	context.beginPath();
+	context.moveTo(canvas.width/2, 450);
+	context.lineTo(canvas.width/2, 750);
+	context.closePath();
+	context.lineWidth = 1;
+	context.stroke();
+	context.restore();
+
 	
 	player.drawRect();
+	ball.drawCircle();
+	p1Wins.fillText();
+	line.drawLine();
 }
 
 
@@ -192,7 +248,17 @@ function showBounce()
 		player.vy = -player.vy * .99;
 	}
 	
+	
+
+
+	
+
+
+
+
 	//-----------------------------------------------------------------------------------------
 }
+
+
 
 
