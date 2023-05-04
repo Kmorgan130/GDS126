c07b2159d7d1bf1a2228971e9c9d756c14cd8255
Your Name <you@example.com>
2023-05-04 15:57:03 -0400

    mini boss

--- Level4_mini_boss/js/basic_platformer.js
+++ Level4_mini_boss/js/basic_platformer.js
@@ -19,9 +19,9 @@ var player;
 		platform0.color = "#66ff33";
 
 		platform1 = new GameObject();
-		platform1.width = 300;
+		platform1.width = 400;
 		platform1.x = canvas.height/2 - 30;
-		platform1.y = canvas.height/2 - 10;
+		platform1.y = canvas.height/2 - 30;
 		platform1.color = "blue";
 
 		platform2 = new GameObject();
@@ -35,13 +35,33 @@ var player;
 		platform3.y = platform2.y- platform0.height/2 - platform1.height/2;
 		platform3.color = "yellow";
 		platform3.vx = 3;
+
+		
+		platform4 = new GameObject();
+		platform4.width = 100;
+		platform4.x = canvas.height/2 + 550;
+		platform4.y = canvas.height/2 - 200;
+		platform4.color = "blue";
+
+		platform5 = new GameObject();
+		platform5.width = 100;
+		platform5.x = canvas.height/2 - 100;
+		platform5.y = canvas.height/2 - 220;
+		platform5.color = "red";
+
+		platform6 = new GameObject();
+		platform6.width = 100;
+		platform6.x = canvas.height/2 + 150;
+		platform6.y = canvas.height/2 - 220;
+		platform6.color = "red";
 		
 	goal = new GameObject({width:24, height:50, x:canvas.width-50, y:100, color:"#00ffff"});
-	
+	goal1 = new GameObject({width:24, height:50, x:20, y:platform0.y-100, color:"#00ffff"});
 
 	var fX = .85;
 	var fY = .97;
 	
+	var key = false;
 	var gravity = 1;
 
 	interval = 1000/60;
@@ -75,7 +95,7 @@ function animate()
 	player.x += Math.round(player.vx);
 	player.y += Math.round(player.vy);
 	
-
+// light green
 	while(platform0.hitTestPoint(player.bottom()) && player.vy >=0)
 	{
 		player.y--;
@@ -98,7 +118,7 @@ function animate()
 		player.vy = 0;
 	}
 
-	//////
+	////// Green
 	while(platform2.hitTestPoint(player.bottom()) && player.vy >=0)
 	{
 		player.y--;
@@ -121,7 +141,7 @@ function animate()
 		player.vy = 0;
 	}
 	
-	//
+	// blue
 	while(platform1.hitTestPoint(player.bottom()) && player.vy >=0)
 	{
 		player.y--;
@@ -143,7 +163,7 @@ function animate()
 		player.y++;
 		player.vy = 0;
 	}
-	///
+	/// Yellow
 
 	while(platform3.hitTestPoint(player.bottom()) && player.vy >=0)
 	{
@@ -165,6 +185,39 @@ function animate()
 		player.y--;
 		player.vy = 0;
 	}
+
+	//goal
+	while(platform4.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	
+	
+	
+// platform 5 red
+	if(platform5.hitTestPoint({x:player.left().x, y:player.bottom().y})&& player.vy >=0 )
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+// goal move right with key
+	while(platform6.hitTestPoint({x:player.right().x, y:player.bottom().y, x:player.right().x}) && player.vx >=0)
+	{
+		player.y--;
+		player.vx = 0;
+		player.canJump = true;
+
+		if(key==true){
+			platform6.x = 10000;
+		}
+	}
+	while(platform6.hitTestPoint(platform4.left()))
+	{
+		platform4.x++;
+	}
 	
 	//---------Objective: Treasure!!!!!!!---------------------------------------------------------------------------------------------------- 
 	//---------Run this program first.
@@ -179,15 +232,32 @@ function animate()
 		context.textAlign = "center";
 		context.drawText("You Win!!!", canvas.width/2, canvas.height/2);
 	}
+
+
+	if(player.hitTestObject(goal1))
+	{
+		goal1.y = player.bottom().y;
+		goal1.x = player.bottom().x;
+		
+	}
+	
+	if(goal1.hitTestObject(platform6))
+	{
+		platform6.x++;
+	}
+	
 	
 	
 	platform0.drawRect();
 	platform1.drawRect();
 	platform2.drawRect();
 	platform3.drawRect();
-
+	platform4.drawRect();
+	platform5.drawRect();
+	platform6.drawRect();
 	//Show hit points
 	player.drawRect();
 	goal.drawCircle();
+	goal1.drawCircle();
 }
 
